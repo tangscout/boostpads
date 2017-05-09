5b3b312dec37d2f54c6c49c245d58f6a977d4ac8
Ken Rachynski <krachynski@ice-edge.com>
2017-05-09 16:35:20 -0600

    Update code
    * added maxBoost config
    * sign check compares to maxBoost
    * fixed flight sound

--- src/main/java/net/scoutlink/boostpads/Main.java
+++ src/main/java/net/scoutlink/boostpads/Main.java
@@ -1,6 +1,7 @@
 package net.scoutlink.boostpads;
 
 import java.io.File;
+
 import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.bukkit.Material;
@@ -17,40 +18,42 @@ import org.bukkit.plugin.java.JavaPlugin;
 import org.bukkit.util.Vector;
 
 public class Main extends JavaPlugin implements Listener {
-    double heightBoost = 1.001D;
-    double dirMultiplier = 2.8D;
+    double heightBoost = 0.201D;
+    double dirMultiplier = 3.8D;
+    double maxBoost = 10.0D;
 
     public Main() {
     }
 
     public void onEnable() {
         Bukkit.getPluginManager().registerEvents(this, this);
-        if(!(new File(this.getDataFolder(), "config.yml")).exists()) {
+        if (!(new File(this.getDataFolder(), "config.yml")).exists()) {
             this.saveDefaultConfig();
         }
 
-        this.heightBoost = this.getConfig().getDouble("HeightBoost");
-        this.dirMultiplier = this.getConfig().getDouble("DirectionMultiplier");
+        this.heightBoost = this.getConfig().getDouble("HeightBoost", heightBoost);
+        this.dirMultiplier = this.getConfig().getDouble("DirectionMultiplier", dirMultiplier);
+        this.maxBoost = this.getConfig().getDouble("MaxBoost", maxBoost);
     }
 
     @EventHandler
     public void onSignChange(SignChangeEvent event) {
         Player p = event.getPlayer();
-        if(event.getLine(0).equalsIgnoreCase("[Boost]")) {
-            if(p.hasPermission("booster.place")) {
-                if(!event.getLine(1).equalsIgnoreCase("") || !event.getLine(2).equalsIgnoreCase("") || !event.getLine(3).equalsIgnoreCase("")) {
+        if (event.getLine(0).equalsIgnoreCase("[Boost]")) {
+            if (p.hasPermission("booster.place")) {
+                if (!event.getLine(1).equalsIgnoreCase("") || !event.getLine(2).equalsIgnoreCase("") || !event.getLine(3).equalsIgnoreCase("")) {
                     try {
                         double x = Double.parseDouble(event.getLine(1));
                         double y = Double.parseDouble(event.getLine(2));
                         double z = Double.parseDouble(event.getLine(3));
-                        if(x <= 5.0D && y <= 5.0D && z <= 5.0D) {
+                        if (x <= maxBoost && y <= maxBoost && z <= maxBoost) {
                             event.setLine(0, ChatColor.GRAY + "[" + ChatColor.DARK_RED + "Boost" + ChatColor.GRAY + "]");
                         } else {
                             event.setCancelled(true);
-                            event.getPlayer().sendMessage(ChatColor.DARK_RED + "Error" + ChatColor.GRAY + ": One of your entered values is too high! None of them should be higher than 4!");
+                            event.getPlayer().sendMessage(ChatColor.DARK_RED + "Error" + ChatColor.GRAY + ": One of your entered values is too high! None of them should be higher than " + String.format("%d", maxBoost) + "!");
                         }
                     } catch (NumberFormatException var9) {
-                        if(!event.getLine(1).equalsIgnoreCase("player")) {
+                        if (!event.getLine(1).equalsIgnoreCase("player")) {
                             event.setCancelled(true);
                             event.getPlayer().sendMessage(ChatColor.DARK_RED + "Error" + ChatColor.GRAY + ": Invalid boost-sign.");
                         } else {
@@ -72,10 +75,10 @@ public class Main extends JavaPlugin implements Listener {
     public void onMove(PlayerMoveEvent event) {
         Player p = event.getPlayer();
         Block block = p.getLocation().add(0.0D, -2.0D, 0.0D).getBlock();
-        if(p.getLocation().add(0.0D, -2.0D, 0.0D).getBlock().getType() == Material.SIGN || p.getLocation().add(0.0D, -2.0D, 0.0D).getBlock().getType() == Material.SIGN_POST || p.getLocation().add(0.0D, -2.0D, 0.0D).getBlock().getType() == Material.WALL_SIGN) {
+        if (p.getLocation().add(0.0D, -2.0D, 0.0D).getBlock().getType() == Material.SIGN || p.getLocation().add(0.0D, -2.0D, 0.0D).getBlock().getType() == Material.SIGN_POST || p.getLocation().add(0.0D, -2.0D, 0.0D).getBlock().getType() == Material.WALL_SIGN) {
             BlockState stateBlock = block.getState();
-            Sign sign = (Sign)stateBlock;
-            if(sign.getLine(0).equals(ChatColor.GRAY + "[" + ChatColor.DARK_RED + "Boost" + ChatColor.GRAY + "]")) {
+            Sign sign = (Sign) stateBlock;
+            if (sign.getLine(0).equals(ChatColor.GRAY + "[" + ChatColor.DARK_RED + "Boost" + ChatColor.GRAY + "]")) {
                 try {
                     double x = Double.parseDouble(sign.getLine(1));
                     double y = Double.parseDouble(sign.getLine(2));
@@ -83,7 +86,7 @@ public class Main extends JavaPlugin implements Listener {
                     p.setVelocity(new Vector(x, y, z));
                     p.playSound(p.getLocation(), Sound.ENTITY_ENDERDRAGON_FLAP, 1.0F, 1.0F);
                 } catch (NumberFormatException var12) {
-                    if(sign.getLine(1).equalsIgnoreCase("player")) {
+                    if (sign.getLine(1).equalsIgnoreCase("player")) {
                         p.setVelocity(p.getLocation().getDirection().setY(this.heightBoost * 0.1D).multiply(this.dirMultiplier));
                     }
                 }
